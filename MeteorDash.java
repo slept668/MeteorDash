e15b24d0de4bb94ccdb071a3a61174028cfa0e96
slept668 <70177580+slept668@users.noreply.github.com>
2024-11-24 18:59:03 -0500

    Update MeteorDash.java
    AssetMan additions

--- MeteorDash.java
+++ MeteorDash.java
@@ -5,23 +5,40 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class MeteorDash extends Game {
 	public SpriteBatch batch;
 	public BitmapFont font;
 	public FitViewport viewport;
-	AudioManager audioMan;
+	public AssetMan assetMan;
+	public AudioManager audioMan;
+	public TextureAtlas mainPack;
+	public TextureAtlas healthPack;
+	public TextureAtlas arrowPack;
 	public Music bgm;
 	
 	public void create() {
 		audioMan = new AudioManager();
-		bgm = Gdx.audio.newMusic(Gdx.files.internal("CanVas - Dead People Faces.mp3"));
-		audioMan.addSound("menuTick", Gdx.audio.newSound(Gdx.files.internal("Sounds/menuTick.mp3")));
-		audioMan.addSound("menuSelect", Gdx.audio.newSound(Gdx.files.internal("Sounds/menuSelect.mp3")));
-		audioMan.addSound("startGame", Gdx.audio.newSound(Gdx.files.internal("Sounds/startGame.mp3")));
-		audioMan.addSound("glass", Gdx.audio.newSound(Gdx.files.internal("sounds/glass.mp3")));
-		audioMan.addSound("bang", Gdx.audio.newSound(Gdx.files.internal("sounds/bang.mp3")));
+		assetMan = new AssetMan();
+		
+		assetMan.loadImages();
+		assetMan.loadMusic();
+		assetMan.loadSounds();
+		assetMan.manager.finishLoading();
+		
+		mainPack = assetMan.manager.get("Pack/MeteorDashMainPack.atlas");
+		healthPack = assetMan.manager.get("Pack/HealthPack.atlas");
+		arrowPack = assetMan.manager.get("Pack/ArrowsPack.atlas");
+		
+		//bgm = Gdx.audio.newMusic(Gdx.files.internal("Music/CanVas - Dead People Faces.mp3"));
+		bgm = assetMan.manager.get("Music/CanVas - Dead People Faces.mp3");
+		//audioMan.addSound("menuTick", Gdx.audio.newSound(Gdx.files.internal("Sounds/menuTick.mp3")));
+		//audioMan.addSound("menuSelect", Gdx.audio.newSound(Gdx.files.internal("Sounds/menuSelect.mp3")));
+		//audioMan.addSound("startGame", Gdx.audio.newSound(Gdx.files.internal("Sounds/startGame.mp3")));
+		//audioMan.addSound("glass", Gdx.audio.newSound(Gdx.files.internal("sounds/glass.mp3")));
+		//audioMan.addSound("bang", Gdx.audio.newSound(Gdx.files.internal("sounds/bang.mp3")));
 		bgm.setLooping(true);
 		bgm.setVolume(0.2f);
 		bgm.play();
@@ -41,11 +58,17 @@ public class MeteorDash extends Game {
 		super.render();
 	}
 	
+	public AssetMan getAssetMan() {
+		return assetMan;
+	}
+	
 	public void dispose() {
+		assetMan.manager.dispose();
 		audioMan.dispose();
 		bgm.dispose();
 		batch.dispose();
 		font.dispose();
 		screen.dispose();
+		System.out.println("MeteorDash.java.dispose()");
 	}
 }
