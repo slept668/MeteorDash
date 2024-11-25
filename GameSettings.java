eae9ffda4674dea453b84e353f05ccea85554f16
Brian <70177580+slept668@users.noreply.github.com>
2024-11-25 13:47:13 -0500

    Update GameSettings.java
    Difficulty changes!

--- GameSettings.java
+++ GameSettings.java
@@ -3,9 +3,11 @@ package io.github.MeteorDash;
 public class GameSettings {
 	private static GameSettings instance;
 	private String difficulty;
+	private int diffNum;
 	
 	public GameSettings() {
 		difficulty = "easy";
+		diffNum = 1;
 	}
 	
 	public static GameSettings getInstance() {
@@ -17,6 +19,34 @@ public class GameSettings {
 	
 	public void setDifficulty(String difficulty) {
 		this.difficulty = difficulty;
+	}
+	
+	public void diffRight() {
+		diffNum++;
+		if (diffNum > 3) {
+			diffNum = 1;
+			setDifficulty("easy");
+		}
+		else if (diffNum == 2) {
+			setDifficulty("medium");
+		}
+		else if (diffNum == 3) {
+			setDifficulty("hard");
+		}
+	}
+	
+	public void diffLeft() {
+		diffNum--;
+		if (diffNum < 1) {
+			diffNum = 3;
+			setDifficulty("hard");
+		}
+		else if (diffNum == 2) {
+			setDifficulty("medium");
+		}
+		else if (diffNum == 1) {
+			setDifficulty("easy");
+		}
 	}
 	
 	public String getDifficulty() {
